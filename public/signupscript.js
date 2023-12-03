a2fe3b46c541464a202f39566054e5d732b28032
Azam Jawad Butt <azamjb123@gmail.com>
2023-12-03 09:09:31 -0500

    final

--- public/signupscript.js
+++ public/signupscript.js
@@ -1,11 +1,10 @@
-
 document.getElementById('signupForm').addEventListener('submit', function(e) {
     e.preventDefault(); 
 
     const username = this.username.value; // getting username value
     const password = this.password.value; // getting password value
 
-    
+
     const url = 'http://localhost:3000/adduser';
 
     fetch(url, { // make post request to adduser endpoint, to store user
@@ -17,10 +16,12 @@ document.getElementById('signupForm').addEventListener('submit', function(e) {
     })
     .then(response => response.text())
     .then(data => {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+        window.location.href = 'notes.html';
         alert(data); // Show response from the server
     })
     .catch(error => {
         console.error('Error:', error);
     });
 });
-
